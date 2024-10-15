a372f771ea815bcd8f10bdd557918f9db425546f
gonzaloriveraf <gonzalo.rivera.91@gmail.com>
2024-10-14 22:09:30 -0300

    remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+/* import vercel from '@astrojs/vercel'; */
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+ /* adapter: vercel({
+    mode: 'standalone' 
+  }),*/
   output: "server",
   integrations: [vue({
     ssr: true
