d69a6cb2bebf9802b4f75ade49b6a4d58855278b
gonzaloriveraf <gonzalo.rivera.91@gmail.com>
2024-10-16 20:15:52 -0300

    host y port

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ export default defineConfig({
   output: "server",
   adapter: node({
     mode: 'standalone', // Modo recomendado para Render
+    host: '0.0.0.0', // Esto es importante
+    port: process.env.PORT || 3000 // Usar variable de entorno PORT
   }),
   integrations: [vue({
     ssr: true
