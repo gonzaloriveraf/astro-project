f444af9ed2dc04c63c25be45cf7225d5f45b84d2
gonzaloriveraf <gonzalo.rivera.91@gmail.com>
2024-10-16 11:50:18 -0300

    ssr render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
+import node from '@astrojs/node';
+
 
 // https://astro.build/config
 export default defineConfig({
-
+  output: "server",
+  adapter: node({
+    mode: 'standalone', // Modo recomendado para Render
+  }),
   integrations: [vue({
     ssr: true
-  })],
-  output: "server"
-
+  })]
 });
